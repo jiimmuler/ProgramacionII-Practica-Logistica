d9258e01fa8cccd3af386ae1fe5a9612c4e3bf68
Jimena Pintos <jimena.pintos@fiuni.edu.py>
2026-04-14 17:39:32 -0300

    Agregue IConectable Actualicé Vehiculo

---
+++ src/IConectable.java
@@ -0,0 +1,2 @@
+public class IConectable {
+}
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,3 +1,22 @@
-public class Vehiculo {
+public abstract class Vehiculo {
+    private int id;
+    private static int nextId=0;
 
+    public Vehiculo(){
+        setId();
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    private void setId(){
+        this.id=getNextId();
+        nextId++;
+    }
+    public static int getNextId(){
+        return nextId;
+    }
+
+    public abstract void patronMovimiento();
 }
